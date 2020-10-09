694259f40dd386c55bdafc6b4128cb6b2ab7889d
unknown <shibilaasees678@gmail.com>
2020-10-09 19:56:10 +0530

    this is frontend

---
+++ luminarpythonprograms/FRONTEND/JAVASCRIPT/java2.js
@@ -0,0 +1,18 @@
+var num=10;
+if(num>0){
+console.log("Positive")
+}
+else if (num<0 ){
+console.log("negative")
+}
+else{
+console.log("zero")
+}
+
+var num1=10,num2=20;
+if(num1>num2){
+console.log("Largest number is:"+num1)
+}
+else{
+console.log("Largest number is:"+num2)
+}
---
+++ luminarpythonprograms/FRONTEND/JAVASCRIPT/javascript.js
@@ -0,0 +1,35 @@
+alert ("hello world");
+
+
+console.log("hello");
+var num1=10;
+var num2=20;
+var sum=num1+num2;
+a=typeof sum
+console.log(a)
+console.log("sum is:"+sum)
+
+numa=Number(prompt("Enter the number:"))
+numb=Number(prompt("enter the number:"))
+var sum2=numa + numb
+alert("sum is:"+sum2)
+
+var sub=num1-num2;
+console.log("substration is:"+sub)
+
+var mul=num1*num2;
+console.log("multiplication is:"+mul)
+
+var div=num1/num2;
+console.log("division is:"+div)
+//boolean and logic
+
+console.log(true&false)
+console.log(true|false)
+console.log(true^false)
+
+
+
+
+
+
